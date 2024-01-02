16cefbfa545acd30113f07ec91a460363d5c9747
Krzysztof.Zawadka <krzysztof.zawadka@pl.markant.com>
2024-01-02 20:18:51 +0100

    Day 4 files

---
+++ src/main/java/com/krizotto/Day4.java
@@ -0,0 +1,10 @@
+public class Day4 {
+    public void solve() {
+        File testFile = new File("src/resources/day4.txt")
+    }
+
+    private Integer solveA(File file) {
+
+        return 0;
+    }
+}
