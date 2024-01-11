13ae2fb3c62ec49898b20fcef173dc5ed7a4dce7
Krzysztof Zawadka <kzawadka@gmail.com>
2024-01-11 16:44:37 +0100

    Day 11 fixed solution

--- src/main/java/com/krizotto/Day11.java
+++ src/main/java/com/krizotto/Day11.java
@@ -8,9 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import org.apache.commons.lang3.StringUtils;
+import java.util.stream.IntStream;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -18,35 +16,15 @@ import lombok.EqualsAndHashCode;
 public class Day11 {
 
     private static List<List<Character>> extract2dList(Path path) throws IOException {
-        List<List<Character>> linesExtended = Files.readAllLines(path)
-                                                   .stream()
-                                                   .flatMap(line -> StringUtils.containsOnly(line, '.') ? Stream.of(line, line) : Stream.of(line))
-                                                   .map(String::trim)
-                                                   .map(String::chars)
-                                                   .map(stream -> stream.mapToObj(c -> (char) c).collect(Collectors.toList()))
-                                                   .collect(Collectors.toList());
-        for (int i = 0; i < linesExtended.getFirst().size(); i++) {
-            final int finalI = i;
-            if (linesExtended.stream().allMatch(list -> list.get(finalI) == '.')) {
-                linesExtended.forEach(line -> line.add(finalI, '.'));
-                i += 1;
-            }
-        }
-        return linesExtended;
+        return Files.readAllLines(path)
+                    .stream()
+                    .map(String::trim)
+                    .map(String::chars)
+                    .map(stream -> stream.mapToObj(c -> (char) c).collect(Collectors.toList()))
+                    .collect(Collectors.toList());
     }
 
-    public void solve() throws IOException {
-        Path test = Paths.get("src/resources/day11_test.txt");
-        Path input = Paths.get("src/resources/day11.txt");
-        System.out.println("Day 10");
-        System.out.printf("Part A (test): %d%n", solveA(test));
-        System.out.printf("Part A: %d%n", solveA(input));
-        //        System.out.printf("Part B (test): %d%n", solveB(test));
-        //        System.out.printf("Part B (test): %d%n%n", solveB(input));
-    }
-
-    private Integer solveA(Path path) throws IOException {
-        List<List<Character>> space = extract2dList(path);
+    private static Set<Point> findGalaxies(List<List<Character>> space) {
         Set<Point> galaxies = new HashSet<>();
         for (int y = 0; y < space.size(); y++) {
             for (int x = 0; x < space.get(y).size(); x++) {
@@ -55,16 +33,39 @@ public class Day11 {
                 }
             }
         }
-        int sum = 0;
-        for (Point galaxy : galaxies) {
-            sum += galaxies.stream().filter(g -> !g.equals(galaxy)).map(g -> Math.abs(g.x - galaxy.x) + Math.abs(g.y - galaxy.y)).reduce(0, Integer::sum);
-        }
+        return galaxies;
+    }
 
+    public void solve() throws IOException {
+        Path test = Paths.get("src/resources/day11_test.txt");
+        Path input = Paths.get("src/resources/day11.txt");
+        System.out.println("Day 10");
+        System.out.printf("Part A (test): %d%n", solve(test, 2));
+        System.out.printf("Part A: %d%n", solve(input, 2));
+        System.out.printf("Part B (test): %d%n", solve(test, 100));
+        System.out.printf("Part B: %d%n%n", solve(input, 1000000));
+    }
+
+    private long solve(Path path, long times) throws IOException {
+        List<List<Character>> space = extract2dList(path);
+        Set<Point> galaxies = findGalaxies(space);
+        List<Integer> blankRows = IntStream.range(0, space.size()).filter(i -> space.get(i).stream().allMatch(character -> character == '.')).boxed().toList();
+        List<Integer> blankCols = IntStream.range(0, space.getFirst().size()).filter(i -> space.stream().allMatch(list -> list.get(i) == '.')).boxed().toList();
+        long sum = 0;
+        for (Point galaxy : galaxies) {
+            sum += galaxies.stream().filter(g -> !g.equals(galaxy)).map(g -> countDistance(galaxy, g, blankRows, blankCols, times)).reduce(0L, Long::sum);
+        }
         return sum / 2;
     }
 
-    private Integer solveB(Path path) throws IOException {
-        return 0;
+    private long countDistance(Point p1, Point p2, List<Integer> blankRows, List<Integer> blankCols, long times) {
+        int maxY = Math.max(p1.y, p2.y);
+        int maxX = Math.max(p1.x, p2.x);
+        int minY = Math.min(p1.y, p2.y);
+        int minX = Math.min(p1.x, p2.x);
+        long addRows = blankRows.stream().filter(i -> i >= minY && i <= maxY).count();
+        long addCols = blankCols.stream().filter(i -> i >= minX && i <= maxX).count();
+        return Math.abs(maxY - minY) + addRows * (times - 1L) + Math.abs(maxX - minX) + addCols * (times - 1L);
     }
 
     @AllArgsConstructor
